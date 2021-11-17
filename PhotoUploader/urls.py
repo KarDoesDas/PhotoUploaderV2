62396d42c92dfaf9a627fd53ab1aa37097d6daf1
Ingwald <Ingwald007@gmail.com>
2021-11-18 00:14:57 +0200

    master

--- PhotoUploader/urls.py
+++ PhotoUploader/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('loadingphoto.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('uploads/', include('uploads.urls')),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
