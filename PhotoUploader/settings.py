3ef61ac54f0fea62e6107cebd253918613d6889c
Ingwald <Ingwald007@gmail.com>
2021-11-22 03:10:05 +0200

    master

--- PhotoUploader/settings.py
+++ PhotoUploader/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=mbe_-hqc)vn%!ii3mxcu&ki%r36gtpvkk=_c=6322^l42@i)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['photouploaderv2.herokuapp.com', '127.0.0.1']
 
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'PhotoUploader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'photouploader',
-#         'USER': 'postgres',
-#         'PASSWORD': "admin",
-#         'HOST': 'localhost'
-
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photouploader',
+        'USER': 'postgres',
+        'PASSWORD': "admin",
+        'HOST': 'localhost'
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
